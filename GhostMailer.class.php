d8695140e44c32d8d8c74b159f617ad218443c62
Nathan Bastiaans <root@nathanbastiaans.nl>
2014-12-04 10:35:22 +0100

    Added the quickSend() function
    The quickSend() function allows you to send emails with less code so you
    have more time for important things, like beer :beer: ! Cheers!
    
    Also made some  small changes to the code layout. It should be more
    readable now...

--- GhostMailer.class.php
+++ GhostMailer.class.php
@@ -59,7 +59,7 @@ class GhostMailer {
     /**
 	 * Sets default values for sending mail.
 	 */	
-	public function __construct() {
+	public function __construct () {
 		
 		$this->setHeaders ( 'MIME-Version',	'1.0' ); 
 		$this->setHeaders ( 'Content-Type',	'text/html; charset=iso-8859-1' ); 
@@ -117,7 +117,7 @@ class GhostMailer {
 	 */
 	public function addRecipient ( $recipient ) {
 
-		array_push($this->recipients, $recipient);
+		array_push( $this->recipients, $recipient );
 	
 	}
 
@@ -138,8 +138,8 @@ class GhostMailer {
 	public function setSender ( $sender ) {
 		
 		$this->sender = $sender; 
-		$this->setHeaders('From', $sender);
-		$this->setHeaders('Reply-To', $sender);
+		$this->setHeaders( 'From', $sender );
+		$this->setHeaders( 'Reply-To', $sender );
 		
 	}
 
@@ -160,7 +160,7 @@ class GhostMailer {
 	public function setReturnAddress ( $address ) {
 		
 		$this->returnAddress = $address; 
-		$this->setHeaders('Return-Path', $address);
+		$this->setHeaders( 'Return-Path', $address );
 		
 	}
 	
@@ -238,18 +238,83 @@ class GhostMailer {
 	/**
 	 * Adds an attachment to the e-mail.
 	 * @param string
+	 * @return bool
 	 */
 	public function addAttachment ( $attachment ) {
 		
 		if( is_file ( $attachment ) ) {
 		
-			array_push($this->attachments, $attachment);
+			array_push( $this->attachments, $attachment );
 			return true;
 			
 		} 
 		
 		return false;
 	
+	}
+	
+	/**
+	 * Sends an email from just one function 
+	 * @param string $to
+	 * @param string $from
+	 * @param string $subject
+	 * @param string $message
+	 * @param array $headers
+	 * @param array $attachments
+	 * @return bool
+	 */
+	public function quickSend ( $to, $from, $subject, $message, $headers = array(), $attachments = array() ) {
+	
+		if( is_array( $to ) ) {
+		
+			foreach( $to as $recipient ) {
+			
+				$this->addRecipient( $recipient );
+			
+			}
+			
+		} else {
+		
+			$this->addRecipient( $to );
+		
+		}
+		
+		$this->setSender( $from );
+		
+		$this->setReturnAddress( $from );
+		
+		$this->setSubject( $subject );
+		
+		$this->setMessage( $message );
+		
+		if( $message != strip_tags( $message ) ) {
+		
+			$this->setHTML( true );
+		
+		}
+		
+		if( count ( $headers ) > 0 ) {
+
+			foreach( $headers as $key => $value ) {
+			
+				$this->setHeaders ( $key, $value );
+			
+			}
+			
+		}
+
+		if( count ( $attachments ) > 0 ) {
+
+			foreach( $attachments as $file ) {
+			
+				$this->addAttachment ( $file ); 
+			
+			}
+			
+		}
+
+		return $this->send();
+		
 	}
 	
 	/**
@@ -260,37 +325,37 @@ class GhostMailer {
 
 		$message	= $this->message;
 		$head		= ""; 
-		foreach($this->header as $key => $value) { $head.= $key . ': ' . $value . $this->eol; }
+		foreach ( $this->header as $key => $value) { $head.= $key . ': ' . $value . $this->eol; }
 
 		if( count( $this->attachments ) > 0 ) {
 			
-			$separator = md5(time());
-			$this->setHeaders('Content-Type', 'multipart/mixed; boundary="' . $separator . '"');
+			$separator = md5( time() );
+			$this->setHeaders( 'Content-Type', 'multipart/mixed; boundary="' . $separator . '"' );
 
 			$head		= ""; 
-			foreach($this->header as $key => $value) { $head.= $key . ': ' . $value . $this->eol; }
+			foreach( $this->header as $key => $value ) { $head.= $key . ': ' . $value . $this->eol; }
 			$head.= "Content-Transfer-Encoding: 7bit" . $this->eol;
 			$head.= "This is a MIME encoded message." . $this->eol . $this->eol;		
 
 			// message
 			$head .= "--" . $separator . $this->eol;
-			$head .= "Content-Type: text/".($this->isHTML ? 'html' : 'plain' )."; charset=\"iso-8859-1\"" . $this->eol;
+			$head .= "Content-Type: text/" . ( $this->isHTML ? 'html' : 'plain' ) . "; charset=\"iso-8859-1\"" . $this->eol;
 			$head .= "Content-Transfer-Encoding: 8bit" . $this->eol . $this->eol;
 			$head .= $message . $this->eol . $this->eol;
 			$head .= "--" . $separator . $this->eol;
 			
 			$message = "";
 			
-			foreach($this->attachments as $attached) {
+			foreach( $this->attachments as $attached ) {
 			
 				$tmp		= explode("/", $attached);
-				$filename	= end($tmp);
+				$filename	= end( $tmp );
 
-				$file_size = filesize($attached);
-				$handle = fopen($attached, "r");
-				$content = fread($handle, $file_size);
-				fclose($handle);
-				$content = chunk_split(base64_encode($content));
+				$file_size = filesize( $attached );
+				$handle = fopen( $attached, "r" );
+				$content = fread( $handle, $file_size );
+				fclose( $handle );
+				$content = chunk_split( base64_encode( $content ) );
 
 				// attachment
 				$head .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $this->eol;
@@ -303,7 +368,7 @@ class GhostMailer {
 			
 		}
 
-		foreach($this->recipients as $recipient) {
+		foreach( $this->recipients as $recipient ) {
 		
 			if( ! mail(
 					$recipient,
